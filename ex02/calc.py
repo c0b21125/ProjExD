26cca6fe1ea83c9530077bdd6b37480d751c88bc
c0b21125 <c0b2112531@edu.teu.ac.jp>
2022-10-04 14:38:36 +0900

    3まで終了

---
+++ ex02/calc.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def num_click(event):
+    btn = event.widget
+    num = int(btn["text"])
+    tkm.showinfo(num, f"{num}のボタンが押されました")
+
+root = tk.Tk()
+root.title("電卓")
+root.geometry("300x500") #練習１
+
+r, c = 0, 0
+for i, num in enumerate(range(9, -1, -1), 1):
+    btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width = 4, height = 2)
+    btn.bind("<1>", num_click)
+    btn.grid(row=r, column=c)
+    c += 1
+    if i % 3 == 0:
+        r += 1
+        c = 0
+
+
+root.mainloop()
