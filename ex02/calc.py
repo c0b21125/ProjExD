e21fa0c56c30c29e11d05ce5a372d2c198d9cdee
c0b21125 <c0b2112531@edu.teu.ac.jp>
2022-10-04 14:42:51 +0900

    3終了

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,6 +10,9 @@ root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500") #練習１
 
+entry = tk.Entry(root, width=10, font=("", 40))
+entry.grid(row=0, column=0, columnspan=3)
+
 r, c = 0, 0
 for i, num in enumerate(range(9, -1, -1), 1):
     btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width = 4, height = 2)
