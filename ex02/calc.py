457f245a6487c1670e8fd4abdcc2563b15da61a0
c0b21125 <c0b2112531@edu.teu.ac.jp>
2022-10-04 15:27:40 +0900

    7終了

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,19 +3,28 @@ import tkinter.messagebox as tkm
 
 def num_click(event):
     btn = event.widget
-    num = int(btn["text"])
+    num = btn["text"]
     #tkm.showinfo(num, f"{num}のボタンが押されました")
     entry.insert(tk.END, num)
+    
+def equal_click(event):
+    eqn = entry.get() # getメソッドは引数いらない
+    res = eval(eqn) # 式の評価
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
 
 root = tk.Tk()
 root.title("電卓")
-root.geometry("300x500") #練習１
+root.geometry("300x700") #練習１
 
 entry = tk.Entry(root, width=10, font=("", 40), justify="right") # 練習4
 entry.grid(row=0, column=0, columnspan=3)
 
 r, c = 1, 0
-for i, num in enumerate(range(9, -1, -1), 1):
+numbers = list(range(9, -1, -1)) # 数字のリスト
+enzanshi = ["+", "-", "*", "/"] # 演算子のリスト
+
+for i, num in enumerate(numbers + enzanshi, 1):
     btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width = 4, height = 2)
     btn.bind("<1>", num_click)
     btn.grid(row=r, column=c)
@@ -24,5 +33,7 @@ for i, num in enumerate(range(9, -1, -1), 1):
         r += 1
         c = 0
 
-
+btn = tk.Button(root, text=f"=", font=("Times New Roman", 30), width = 4, height = 2)
+btn.bind("<1>", equal_click)
+btn.grid(row=r, column=c)
 root.mainloop()
