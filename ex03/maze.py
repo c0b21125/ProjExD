06f1d8bbdcb838702f38ecbc8320d8e6dfdcffb6
c0b21125 <c0b2112531@edu.teu.ac.jp>
2022-10-18 14:09:09 +0900

    練習2

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,4 +4,9 @@ import tkinter.messagebox as tkm
 if __name__ == "__main__":
     root = tk.Tk() # 練習１
     root.title("迷えるこうかとん")
+
+    # キャンバスのサイズ設定
+    canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
+    canvas.pack() # 練習２
+
     root.mainloop()
