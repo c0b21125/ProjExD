d5a71fd92e57f009d731312845e4f38c61c9e5ee
c0b21125 <c0b2112531@edu.teu.ac.jp>
2022-10-25 15:36:14 +0900

    練習04-7まで

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,6 +2,20 @@ import pygame as pg
 import sys
 from random import randint
 
+def check_bound(obj_rct, scr_rct):
+    """
+    obj_rct：こうかとんrct、爆弾rct
+    scr_rct：スクリーンrct
+    領域内：-1/領域外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん") # 練習１
     scrn_sfc = pg.display.set_mode((1600, 900))
@@ -46,9 +60,24 @@ def main():
             tori_rct.centerx -= 1
         if key_states[pg.K_RIGHT]: # 横座標を+1
             tori_rct.centerx += 1
+        yoko, tate = check_bound(tori_rct, scrn_rct)
+        if yoko == -1:
+            if key_states[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_states[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+
+        if tate == -1:
+            if key_states[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_states[pg.K_DOWN]:
+                tori_rct.centery -= 1
 
         scrn_sfc.blit(tori_sfc, tori_rct) # 練習３
 
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
         bomb_rct.move_ip(vx, vy) # 練習６
         scrn_sfc.blit(bomb_sfc, bomb_rct) # 練習５
 
